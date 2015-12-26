f9f6897fd0f2c392ee3c48f869d5fc045f224e7e
KrupaR <Krupa.Rajendran@gmail.com>
2015-12-26 22:10:33 +0530

    Assignment: Caching the Inverse of a Matrix
    Course Assignment

---
+++ makeCacheMatrix.R
@@ -0,0 +1,42 @@
+## makeCacheMatrix creates a list containing the following function
+##set the value of the vector
+##get the value of the vector
+##set the value of the mean
+##get the value of the mean
+
+makeCacheMatrix <- function(x = matrix()) {
+    invMatrix <- NULL
+    set <- function(y) {
+        x <<- y
+        invMatrix <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) invMatrix <<- inverse
+    getinverse <- function() invMatrix
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+}
+
+
+## cacheSolve computes the inverse of the matrix returned by makeCacheMatrix. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+    invMatrix <- x$getinverse()
+    if(!is.null(invMatrix)) {
+        message("Fetching from cached data")
+        return(invMatrix)
+    }
+    data <- x$get()
+    invMatrix <- solve(data)
+    x$setinverse(invMatrix)
+    invMatrix
+}
+
+
+##Inputs for testing
+##my_matrix <- matrix(rnorm(16),nrow=4,ncol=4)
+##my_inv_matrix= makeCacheMatrix(my_matrix)
+##my_inv_matrix$get()
+##cacheSolve(my_inv_matrix)
+##cacheSolve(my_inv_matrix)
